5de37712bd35a8a87faba33091081fcffc664cf4
lauraAgatha <laura.bosdriesz@gmail.com>
2021-06-08 17:36:46 +0200

    Second try files assignment

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ import os
 import shutil
 import zipfile
 
-cache_dir = r'D:\Documenten\Trainingen\Wincademy\files\cache'
+cache_dir = os.path.abspath("cache")
+# cache_dir = r'D:\Documenten\Trainingen\Wincademy\files\cache'
 zip_path = r'D:\Documenten\Trainingen\Wincademy\files\data.zip'
 
 # Excercise 1
 def clean_cache():
-
-    if not os.path.exists(cache_dir):
-        os.makedirs(cache_dir)
-        print("Created Directory : ", cache_dir)
-    else:
+    if os.path.exists(cache_dir):
         shutil.rmtree(cache_dir)
         print("Directory already existed : ", dir)
-
+    else:
+        os.makedirs(cache_dir)
+        print("Created Directory : ", cache_dir)
 
 clean_cache()
 
@@ -26,31 +25,31 @@ def cache_zip(zip_path, cache_dir):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(cache_dir)
 
-
 print(cache_zip(zip_path, cache_dir))
 
 # Excercise 3
 def cached_files():
-    abs_cache_dir = os.path.abspath(cache_dir)
     cached_files = []
-    for file in os.listdir(abs_cache_dir):
-        cached_files.append(f"{abs_cache_dir}\{file}")
-    print(cached_files)
+    for file in os.listdir(cache_dir):
+        cached_files.append(f"{cache_dir}\{file}")
     return cached_files
-
 
 cached_files()
 
+
+# PLEASE READ THIS COMMENT:
+# I adjusted this assignment with your feedback and you explained to change the forloop as in the comments, which was what I did.
+# In both versions I got the password printed if you do print(password), but I still get thumbs down for the last find password.
 # Excercise 4
 def find_password(cached_files):
     search_pw = "password"
     for file in cached_files:
         f = open(file, "r")
-        for line in f:
+        # for line in f:
+        for line in f.readlines():
             password = None
             if search_pw in line:
                 password = line.split(": ")[1]
-                #print(password)
                 return password
 
 find_password(cached_files())
